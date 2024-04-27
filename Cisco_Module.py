ef5c3acbad12dc9c4d43353159e9d7a98116b425
Codester3310 <cody.turek@hotmail.com>
2024-04-27 04:30:35 -0600

    + Added Range module integration to main program + Added exception handling for FileNotFound + Added other exception handlers + Restructured Cisco_Module.output dictionary serialization process - Removed unused code - Removed useless test .json files from commit = Squashed CIDER notation related bugs

--- Cisco_Module.py
+++ Cisco_Module.py
@@ -1,15 +1,23 @@
 import json
 class cisco:
-    '''An object used to keep track of ip addressing and commands'''
-    def __init__(self):
+    '''An object used to keep track of Cisco IOS commands'''
+    def __init__(self, keywords):
+        self.keywords = keywords
         self.ip_addr = ''
         self.subnet = ''
         self.vlan_name = ''
         self.interface_id = ''
         self.vlan_id = 1
         self.i = ''
-        self.interfacerange = {}
+        self.sw_mode = ''
+        self.vlan = ''
+        self.interface = ''
+
         
+        self.cisco_dict = {} # <******** This is the root dictionary ***********
+        self.vlan_dict = {} # Vlan Dictionary
+        self.sw_mode_dict = {} # Switchport Mode dictionary
+        self.interface_dict = {} # Interface dictionary
         
     def vlan(self, **vlan_id):
         '''
@@ -20,19 +28,21 @@ class cisco:
         
         while self.i != 'r':
             try:
-                self.vlan_id = int(input("Enter the vlan id"))
+                self.vlan_id = int(input("Enter the vlan id\n"))
                 if self.vlan_id > 1 and self.vlan_id < 999:
                     self.i = 'r'
                     self.vlan = True
                 else:
                     raise TypeError("Invalid entry. Vlan must be an integer.")
             except TypeError:
-                vlan_id = int(input("What VLAN will you use?"))
+                self.i = ''
+                self.vlan_id = int(input("What VLAN will you use? (enter ID) \n"))
+                continue
             except:
                 print("Error. Unhandled exception in vlan module.")
                 print("Error at vlan_id value entry.")
             try:
-                self.vlan_name == input("What is the Vlan name?")
+                self.vlan_name = input("What is the Vlan name? \n")
                 if isinstance(self.vlan_name, str):
                     pass
                 else:
@@ -40,6 +50,33 @@ class cisco:
             except :
                 print("Error. Unhandled exception in vlan module.")
                 print("Error at vlan_name value entry.")
+ 
+            
+    def port_range(self, interface_sgfa, int_range=1): 
+        ''' 
+        Second parameter is used to take in whether the user wants
+        Gigabit, FastEthernet, or Serial interface types
+
+        Third Parameter takes the number of interfaces that need to be configured
+
+        This module takes in a range of interfaces and saves the proper commands
+        to a dictionary
+        '''
+        try:
+            for i in range(self.int_range):
+                if interface_sgfa.lower() in ['S', 'S0/0', 'S0', 'Serial', 'ser']:
+                    i -= 1
+                    self.command[i] = f"interface range serial0/{i} - {int_range}"
+                    self.interface_dict["Interface range and type" + i] = (f"interface range serial0/{i} - {int_range}")
+                elif interface_sgfa.lower() in ['f', 'fa', 'fast', 'fastethernet', 'fa0', 'fa0/0', 'fa0/0.0']:
+                    self.interface_dict["Interface range and type" + i] = f"interface range FastEthernet0/{i} - {int_range}"
+                elif interface_sgfa.lower() in ['g', 'gi', 'giga', 'gigabit', 'g0', 'g0/0', 'g0/0.0']:
+                    self.interface_dict["Interface range and type" + i] = f"interface range GigabitEthernet0/{i} - {int_range}"    
+        except ValueError:
+            print("ERROR. Invalid range.")
+        finally:
+            print("Range commands saved.")
+
 
 
     def output(self):
@@ -47,65 +84,45 @@ class cisco:
         This module prints and outputs all of the 
         commands from the instance to a file and the shell
         '''
-        try:
-            if vlan == True:
-                print(f"vlan {self.vlan_id}")
-                print(f"name {self.vlan_name}")
-                print(f"int {self.interface_id}")
+        if self.vlan == True:
+            print(f"vlan {self.vlan_id}")
+            print(f"name {self.vlan_name}")
+            # print(f"int {self.interface_id}")
+            self.vlan_dict["vlan_id"] = f"vlan {self.vlan_id}"
+            self.vlan_dict["name"] = f"name {self.vlan_name}"
+            # self.vlan_dict["interface"] = f"int {self.interface_id}"
 
-                
-                try:
-                    self.interface_id = input("Enter the Interface number (g0/0, fa0/0, etc.).")
-                    if isinstance(self.interface_id, str):
-                        pass
-                    else:
-                        raise ValueError
-                except ValueError:
-                    print("Error, value entered is not a string.\n"
-                            "remember to include the Gigabit or FastEthernet\n"
-                            "interface portion of the str.")
-                    
-                if self.sw_mode == True:
-                    print(f"switchport mode {self.sw_mode}") # Write a line to capture if interface is trunk or access port
-                    print(f"switchport access vlan {self.vlan_id}")
-                
-                else:
-                    pass
-            else:
-                raise ValueError("Missing data from printed statement.")
-        except ValueError:
-            if self.vlan == False:
-                if vlan_id == 1:
-                        self.vlan_id = int(input("Enter your VLAN ID.\n"))
-                elif self.vlan_name == False:
-                    self.vlan_name = input("Enter your vlan name.\n")
-            else:
-                self.interface_id = False
-        finally:
-            print("exit")
-            
-        def port_range(self, interface_sgfa, int_range='0/0 - 5'): 
-            ''' 
-            Second parameter is used to take in whether the user wants
-            Gigabit, FastEthernet, or Serial interface types
+            self.cisco_dict["vlan commands"] = self.vlan_dict 
 
-            This module takes in a range of interfaces and saves the proper commands
-            to a dictionary
+        if self.sw_mode == True :
+            print(f"switchport mode {self.sw_mode}") # Write a line to capture if interface is trunk or access port
+            print(f"switchport access vlan {self.vlan_id}")
+
+            self.sw_mode_dict["Switchport mode"] = (f"switchport mode {self.sw_mode}")
+            if self.vlan == True:
+                self.sw_mode_dict["Switchport mode"] = (f"switchport access vlan {self.vlan_id}")
+                self.cisco_dict["Switchport Modes"] = self.sw_mode_dict
+        
+        if self.interface_id == True: 
+            '''Checks to see if Interface 
+                commands were entered and saves to a dictionary
             '''
-            try:
-                for i in range(self.int_range):
-                    if interface_sgfa.lower() in ['S', 'S0/0', 'S0', 'Serial', 'ser']:
-                        i -= 1
-                        self.command[i] = f"interface range serial0/{i} - {range}"
-                        self.interface_dictionary.append(f"interface range serial0/{i} - {range}")
-                    elif interface_sgfa.lower() in ['f', 'fa', 'fast', 'fastethernet', 'fa0', 'fa0/0', 'fa0/0.0']:
-                        self.command[i] = f"interface range FastEthernet0/{i} - {range}"
-                    elif interface_sgfa.lower() in ['g', 'gi', 'giga', 'gigabit', 'g0', 'g0/0', 'g0/0.0']:
-                        self.command[i] = f"interface range GigabitEthernet0/{i} - {range}"    
-            except ValueError:
-                print("ERROR. Invalid range.")
-            finally:
-                print("Range commands saved.")
+            for i in range(int_range):
+                self.cisco_dict["Int Range" + i] = self.interface_dict["Interface range and type" + i]
 
 
+            print("exit")
+        return self.cisco_dict
 
+# ------------------------------ CODE ABYSS --------------------------------------- #
+        # try:
+        #     self.interface_id = input("Enter the Interface number (g0/0, fa0/0, etc.)\n")
+        #     if isinstance(self.interface_id, str):
+        #         pass
+        #     else:
+        #         raise ValueError
+        # except ValueError:
+        #     print("Error, value entered is not a string.\n"
+        #             "remember to include the Gigabit or FastEthernet\n"
+        #             "interface portion of the str.")
+            
--- Final-Project IPv6 transcriber.py
+++ Final-Project IPv6 transcriber.py
@@ -1,39 +1,67 @@
-import json, math, Cisco_Module
+import json, math, Cisco_Module, re
 # Save key names to key values in dictionary
 
     # Asks for string to be used in major portions of algorithm
 # network_address = input("Please enter your Global Routing IP address (include colons).\n")
 network_address = "2001:0451:0051" # Placeholder
-global_prefix_cider = ''
 host_segment = '0000:0000:0000:0000'
 c = False
+global_prefix_cider = ''
+truncated_list = []
+segments_dict = {}
+_ = ''
 # Determines notation and format of output
 
 
 def ver_cid_variable_forager():      
     '''IMPORTANT-- this function grabs all variables required for CIDER and Verbose formats'''
-    global v6_Seg_Count, subnet_address, host_estimate, total_estimate,host_distribute, ip_file_path, segments_dict, truncated_list             
-#   Asks for variables that determine range and loop iterations
-    v6_Seg_Count = int(input("How many subnet segments do you want?\n"))
+    global v6_Seg_Count, subnet_address, host_estimate, total_estimate,host_distribute, ip_file_path, truncated_list, subnet_address             
+    c = False
+    manual_Even_Toggle = ["manually", "manual", "man", "m", "evenly", "even", "eve", "ev", "e"]
+    while c == False:
+    #   Asks for variables that determine range and loop iterations
+        try: # Gets total estimate and handles value errors
+            v6_Seg_Count = int(input("How many subnet segments do you want?\n"))
+            if isinstance(v6_Seg_Count, int):
+
+                host_estimate = int(input("How many hosts do you expect to use your network (total)?\n"))
+                if isinstance(host_estimate, int):
+                        
+                    total_estimate = host_estimate + v6_Seg_Count
+                    if not isinstance(total_estimate, int):
+                        raise ValueError
+                else:
+                    raise ValueError
+            else:
+                raise ValueError 
+        except ValueError:
+            print("Invalid entry. Please enter an integer")
+            continue
+
+    # -----------------------------------------------------------
+
+        try:        
+            # Case variable to track how to calculate the print out formula
+            host_distribute = input("Manually assign host count per segment or distrbute hosts evenly?\n")
+            if isinstance(host_distribute, str) and host_distribute.lower() in manual_Even_Toggle:
+                
+                        # Asks for file path information
+                ip_file_path = input(r"Enter your file name (path optional)." + "\n") 
+                if isinstance(ip_file_path, str):
+                    c = True
+                else:
+                    raise ValueError
+            else:
+                raise ValueError 
+        except ValueError:
+            print("Invalid entry. Please enter a string.")
+            continue
 
     ''' subnet_address is the largest subnet addr defined. 
         Increment downwards till zero to use all subnets
         OR increment from zero till your index is greater than subnet_address
     '''
     subnet_address = (f"{v6_Seg_Count:04x}") 
-
-    host_estimate = int(input("How many hosts do you expect to use your network (total)?\n"))
-    total_estimate = host_estimate + v6_Seg_Count
-
-    # Case variable to track how to calculate the print out formula
-    host_distribute = input("Manually assign host count per segment or distrbute hosts evenly?\n")
-
-    # Asks for output information
-    ip_file_path = input(r"Enter your file name (path optional)." + "\n")
-
-    truncated_list = []
-    segments_dict = {}
-    _ = ''
 
 def write_ipv6_to_json(segmentsW): # Write file function
     '''
@@ -47,7 +75,7 @@ def write_ipv6_to_json(segmentsW): # Write file function
     '''
 
     with open( file=ip_file_path, # Opens file to write and Designates the writing filepath 
-        mode='w', encoding=object) as file_out_obj: # file_out_obj is a placeholder variable
+        mode='w') as file_out_obj: # file_out_obj is a placeholder variable
         json.dump(segmentsW, file_out_obj, indent=4)
         file_out_obj.write('\n')  # Add a newline after each entry
         # Still need to write function component that exports all data to save file
@@ -60,7 +88,7 @@ def write_ipv6_to_json(segmentsW): # Write file function
 def format_verbose (): # Defines verbose conversion format
 # this loop saves addresses to the segments dictionary
     ver_cid_variable_forager()
-    for i in (v6_Seg_Count):
+    for i in range(v6_Seg_Count):
 
         key = f"subnet_{i}".format(i+1)
         value = f"value_{i}".format(i+1)
@@ -72,13 +100,15 @@ def format_verbose (): # Defines verbose conversion format
 def format_cisco():
     keywords = []
     cisco_command_dict = {}
-    while 'q' or 'quit' != keywords:
-        keywords.append((input("Input a command (ex. Vlan, range, q for quit)")))
-        if 'q' or 'quit' == keywords:
+    interface_type = 'fa0/0'
+    while True:
+        user_input = (input("Input a command (ex. Vlan, range, q for quit)\n")) 
+        keywords.append(user_input) # input Checker
+        if user_input.lower() in ("q", "quit"):
             return cisco_command_dict
-        cisco = Cisco_Module.cisco() # Assigns the variable 'cisco' to an instance of the Cisco class
+        cisco = Cisco_Module.cisco(keywords) # Assigns the variable 'cisco' to an instance of the Cisco class
         try:
-            routers = int(input("How many routers are you configuring?"))
+            routers = int(input("How many routers are you configuring?\n"))
             if isinstance(routers, int):
                     for i in range(routers):
 
@@ -86,13 +116,29 @@ def format_cisco():
                         if "vlan" in keywords:
                             
                             # Requests Vlan ID from terminal
-                            cisco_command_dict[i] =  cisco.vlan() # 
+                            cisco.vlan()
+                        
+                        if "range" in keywords:
+                            try:
+                                interface_total = int(input("What is your port density (single integers 0 to 384)\n"))
+                                if isinstance(interface_total, int):
+                                    interface_type = input("What kind of port are you configuring? (Serial, Gigabit, FastEthernet)\n")
+                                    cisco.port_range(interface_type, interface_total)
+
+                                else:
+                                    raise ValueError("Invalid entry. Enter the number of interfaces")
+                            except ValueError:
+                                continue
+
+
                 # Defines Cisco conversion function. This runs a for loop to save ipv6 host addresses in a cisco router format
             else: raise ValueError
         except ValueError:
             print("Invalid value entered.") 
-
-    return cisco_command_dict
+        finally:
+            # Saves output
+            cisco_command_dict["Vlan{i}"] = cisco.output()             
+            return cisco_command_dict
 
 def cisco_switch_commands():
     '''
@@ -111,13 +157,26 @@ def cisco_save(save_data, f):
     '''need to write this to be the final function that saves dictionary in cisco format
         to a dict and finishes by running write_ipv6_to_json() function.
     '''
-    with open( file=ip_file_path, # Opens file to write and Designates the writing filepath 
-        mode='a+', encoding=object) as f: # file_out_obj is a placeholder variable
-        json.dump(save_data, f, indent=4)
-        f.write('\n')  # Add a newline after each entry
-        # TODO write save file in txt format
-    # save_data = Cisco_Module.cisco()
-    # save_data[x].port_range(s, int(5))
+    o == True
+    while o == True:
+        global ip_file_path
+        try:
+            if ip_file_path == True:    
+                with open( file=ip_file_path, # Opens file to write and Designates the writing filepath 
+                    mode='a+') as f: # file_out_obj is a placeholder variable
+                    json.dump(save_data, f, indent=4)
+                    f.write('\n')  # Add a newline after each entry
+                    o = False
+            else:
+                raise FileNotFoundError
+        except FileNotFoundError:
+            ip_file_path = "Untitled_Cisco_Commands.json"
+            o = True
+            continue
+                # TODO write save file in txt format
+            # save_data = Cisco_Module.cisco()
+            # save_data[x].port_range(s, int(5))
+
 
 
 
@@ -129,7 +188,6 @@ def format_cider():
             **or fixture uses the same or different trunkation format
             **before plug-and-playing .json file     
     '''
-    ver_cid_variable_forager()
     address_prefix = prefix_length(total_estimate)
     seg0, seg1, seg2 = cider_slice() # Slices the global address into hextets   
     truncated_list = cider_trunc(seg0, seg1, seg2) # trunkates variables from leading zeros
@@ -160,7 +218,7 @@ def cider_trunc(seg0, seg1, seg2):
     
     hextet_vars = [seg0, seg1, seg2] # Puts arguments into a list
 
-    for i in (hextet_vars):
+    for i in range(len((hextet_vars))):
         # Check for at least two characters and two leading zeros
         if len(hextet_vars[i]) > 1 and hextet_vars[i].startswith('0'):  
             while len(hextet_vars[i]) > 1 and hextet_vars[i].startswith('0'):
@@ -202,10 +260,11 @@ def prefix_length(total_address_count):
 def cider_batch_addr_write(num_segments,num_hosts_per_segment ):
     # joins the global prefix with the host bits segment
      # increments the full segments
-
+    segment_data = {}
+    key = "element"
+    segments_dict = {}
     for i in range((num_segments)):
-        segments_dict = (f"{global_prefix_cider}{int(i):04x}")  # Format segment ID with leading zeros
-        segment_data = {}
+        segments_dict[key] = (f"{int(i):04x}")  # Format segment ID with leading zeros
         for j in range((num_hosts_per_segment)):  
             cc = int(j) +1
             cc = str(cc)
@@ -213,7 +272,7 @@ def cider_batch_addr_write(num_segments,num_hosts_per_segment ):
             j = int(j)
             address = f"{global_prefix_cider}::{j+1:04x}"  # Format host address with leading zeros
             segment_data[host] = address
-        segments_dict[global_prefix_cider] = segment_data
+            segments_dict[key] = segment_data
     return segments_dict   
 
 
@@ -236,7 +295,7 @@ def notation_select(notation): # Selects output write format
             Args:
 
             '''
-            global file_out_obj
+            global file_out_obj, ip_file_path
             file_out_obj = format_cisco()
             ip_file_path = input(r"Enter your file name (path optional)." + "\n")
             cisco_save(file_out_obj, ip_file_path) 
@@ -245,6 +304,7 @@ def notation_select(notation): # Selects output write format
             '''
             This prints out in CIDER format, using ipv6 CIDR notation
             '''
+            ver_cid_variable_forager()
             address_prefix = prefix_length(total_estimate) # calculates prefix /xx variable
             format_cider()
             write_ipv6_to_json(truncated_list)      
@@ -268,7 +328,7 @@ def __main__():
             else:
                 raise ValueError
         except ValueError:
-            print("Invalid entry. Please enter the correct format. (Value Error)")
+            print("Invalid entry. Please enter the correct format. (Value Error)\n\n")
 
 
 
